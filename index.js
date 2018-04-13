4c2d3968f1a0de39e7087b64cbc424b1a96951d3
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-13 16:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
