0463fffbf898050ee13841df8fbfe25da99cfb3a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-13 17:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,5 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 function removeLastKitten(){
-  kittens.slice(0, kittens.length - 1)
-  return kittens
+  return kittens.slice(0, kittens.length - 1)
 }
